f4cf7c351626d179862d483a5014795520c38112
RobinKarlsen <robikar91@gmail.com>
2014-04-30 11:27:11 +0200

    Added first two commits from peacekeeper_unni

--- Peacekeeper/src/hiof/android14/group26/peacekeeper/CreateAccountActivity.java
+++ Peacekeeper/src/hiof/android14/group26/peacekeeper/CreateAccountActivity.java
@@ -1,13 +1,22 @@
 package hiof.android14.group26.peacekeeper;
 
 import android.os.Bundle;
+import android.view.View;
 import android.app.Activity;
+import android.content.Intent;
 
 public class CreateAccountActivity extends Activity {
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+
 		setContentView(R.layout.create_account);
+	}
+
+	public void startIntroGroupActivity(View view) {
+		Intent intent = new Intent(this, IntroGroupActivity.class);
+
+		startActivity(intent);
 	}
 }
--- Peacekeeper/src/hiof/android14/group26/peacekeeper/LogInActivity.java
+++ Peacekeeper/src/hiof/android14/group26/peacekeeper/LogInActivity.java
@@ -1,13 +1,23 @@
 package hiof.android14.group26.peacekeeper;
 
 import android.os.Bundle;
+import android.view.View;
 import android.app.Activity;
+import android.content.Intent;
 
 public class LogInActivity extends Activity {
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+
 		setContentView(R.layout.login_account);
 	}
+
+	public void startLoggedInActivity(View view) {
+		Intent intent = new Intent(this, MainActivity.class);
+
+		startActivity(intent);
+	}
+
 }
